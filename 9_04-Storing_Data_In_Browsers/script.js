689e704dcdd21bd2c510b297497ee2b54497e1e3
Alex Graalum <alexgraalum@yahoo.com>
2024-07-30 14:08:28 -0700

    Data Storage Complete

--- 9_04-Storing_Data_In_Browsers/script.js
+++ 9_04-Storing_Data_In_Browsers/script.js
@@ -1,19 +1,42 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
+document.addEventListener("DOMContentLoaded", function () {
 	const noteContainer = document.getElementById("note-container");
 	const newNoteButton = document.getElementById("new-note-button");
 	const colorForm = document.getElementById("color-form");
 	const colorInput = document.getElementById("color-input");
 
-	// TODO: Load the note color from the local storage.
-	let noteColor = null; // Stores the selected note color from the form.
-	// TODO: Load the note ID counter from the local storage.
-	let noteIdCounter = 0; // Counter for assigning unique IDs to new notes.
+	let noteColor = localStorage.getItem("noteColor") || 0; // Stores the selected note color from the form.
+	let noteIdCounter = localStorage.getItem("noteIdCounter") || 0; // Counter for assigning unique IDs to new notes.
 
-	// TODO: Load the notes from the local storage.
+	function loadNotes() {
+		let notes = localStorage.getItem("notes");
+		if (notes == null) {
+			notes = [];
+		} else {
+			notes = JSON.parse(notes);
+		}
+		return notes;
+	}
 
-	function addNewNote ()
-	{
+	function appendNotes() {
+		let notes = loadNotes();
+
+		for (n of notes) {
+			let note = document.createElement("textarea");
+			note.setAttribute("data-note-id", n.id.toString());
+			note.value = n.content;
+			note.className = "note";
+			note.style.backgroundColor = noteColor;
+			noteContainer.appendChild(note);
+		}
+	}
+
+	function saveNotes(notes) {
+		localStorage.setItem("notes", JSON.stringify(notes));
+	}
+
+	appendNotes();
+
+	function addNewNote() {
 		const id = noteIdCounter;
 		const content = `Note ${id}`;
 
@@ -26,18 +49,19 @@ document.addEventListener("DOMContentLoaded", function ()
 
 		noteIdCounter++; // Increments the counter since the ID is used for this note.
 
-		// TODO: Add new note to the saved notes in the local storage.
+		let notes = loadNotes();
+		notes.push({ id, content });
+		saveNotes(notes);
+		localStorage.setItem("noteIdCounter", noteIdCounter.toString());
 	}
 
-	colorForm.addEventListener("submit", function (event)
-	{
+	colorForm.addEventListener("submit", function (event) {
 		event.preventDefault(); // Prevents the default event.
 
 		const newColor = colorInput.value.trim();  // Removes whitespaces.
 
 		const notes = document.querySelectorAll(".note");
-		for (const note of notes)
-		{
+		for (const note of notes) {
 			note.style.backgroundColor = newColor;
 		}
 
@@ -45,43 +69,48 @@ document.addEventListener("DOMContentLoaded", function ()
 
 		noteColor = newColor; // Updates the stored note color with the new selection.
 
-		// TODO: Update the note color in the local storage.
+		localStorage.setItem("noteColor", noteColor);
 	});
 
-	newNoteButton.addEventListener("click", function ()
-	{
+	newNoteButton.addEventListener("click", function () {
 		addNewNote();
 	});
 
-	document.addEventListener("dblclick", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
+	document.addEventListener("dblclick", function (event) {
+		if (event.target.classList.contains("note")) {
 			event.target.remove(); // Removes the clicked note.
 
-			// TODO: Delete the note from the saved notes in the local storage.
+			let id = Number(event.target.getAttribute("data-note-id"));
+			let notes = loadNotes();
+			for (let n = 0; n < notes.length; n++) {
+				if (notes[n].id == id)
+					notes.splice(n, 1);
+			}
+			saveNotes(notes);
 		}
 	});
 
-	noteContainer.addEventListener("blur", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
-			// TODO: Update the note from the saved notes in the local storage.
+	noteContainer.addEventListener("blur", function (event) {
+		if (event.target.classList.contains("note")) {
+			let id = Number(event.target.getAttribute("data-note-id"));
+			let notes = loadNotes();
+
+			for (let n = 0; n < notes.length; n++) {
+				if (notes[n].id == id)
+					notes[n].content = event.target.value;
+			}
+			saveNotes(notes);
 		}
 	}, true);
 
-	window.addEventListener("keydown", function (event)
-	{
+	window.addEventListener("keydown", function (event) {
 		/* Ignores key presses made for color and note content inputs. */
-		if (event.target.id === "color-input" || event.target.type === "textarea")
-		{
+		if (event.target.id === "color-input" || event.target.type === "textarea") {
 			return;
 		}
 
 		/* Adds a new note when the "n" key is pressed. */
-		if (event.key === "n" || event.key === "N")
-		{
+		if (event.key === "n" || event.key === "N") {
 			addNewNote(); // Adds a new note.
 		}
 	});
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Springboard Software Engineering Career Track: Section 9 - JavaScript &amp; the Web
 
 ---
-| Subsection | Title                                                          | Status             |
-| ---------- | -------------------------------------------------------------- | ------------------ |
-| 9.2        | [The Document Object Model](./9_02-The_Document_Object_Model/) | <b>Complete</b>    |
-| 9.3        | [Events](./9_03-Events/)                                       | <b>Complete</b>    |
-| 9.4        | [Storing Data In Browsers](./9_04-Storing_Data_In_Browsers/)   | <b>In-Progress</b> |
+| Subsection | Title                                                          | Status          |
+| ---------- | -------------------------------------------------------------- | --------------- |
+| 9.2        | [The Document Object Model](./9_02-The_Document_Object_Model/) | <b>Complete</b> |
+| 9.3        | [Events](./9_03-Events/)                                       | <b>Complete</b> |
+| 9.4        | [Storing Data In Browsers](./9_04-Storing_Data_In_Browsers/)   | <b>Complete</b> |
